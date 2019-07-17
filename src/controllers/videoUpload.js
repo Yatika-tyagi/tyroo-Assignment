71e464b7faec638c6847206beaaaf70a98b83f00
yatika tyagi <yatika.tyagi@kelltontech.com>
2019-07-17 20:09:49 +0530

    video upload

--- src/controllers/videoUpload.js
+++ src/controllers/videoUpload.js
@@ -2,43 +2,12 @@ var multer  = require('multer');
 // var upload = multer();
 var videoStitch = require('video-stitch');
 var videoMerge = videoStitch.merge;
+var videoCut = videoStitch.cut;
 const { getVideoDurationInSeconds } = require('get-video-duration')
 
   
 /* MULTER IMPLEMENTATION */
 exports.uploadDoc = (req, res) => {
-  console.log("***", getVideoDurationInSeconds('src/assests/SampleVideo_1280x720_5mb.mp4'))
-  getVideoDurationInSeconds('src/assests/SampleVideo_1280x720_5mb.mp4').then((duration) => {
-    return duration;
-  }).then((duration) => {
-    videoMerge()
-    .original({
-      "fileName": 'src/assests/SampleVideo_1280x720_5mb.mp4',
-      "duration": duration,
-    })
-    .clips([
-      {
-        "startTime": "hh:mm:ss",
-        "fileName": "FILENAME",
-        "duration": "hh:mm:ss"
-      },
-      {
-        "startTime": "hh:mm:ss",
-        "fileName": "FILENAME",
-        "duration": "hh:mm:ss"
-      },
-      {
-        "startTime": "hh:mm:ss",
-        "fileName": "FILENAME",
-        "duration": "hh:mm:ss"
-      }
-    ])
-    .merge()
-    .then((outputFile) => {
-      console.log('path to output file', outputFile);
-    });
-
-  })
   var storage = multer.diskStorage({
     destination: function (request, file, callback) {
       callback(null, './src/assests');
@@ -47,16 +16,56 @@ exports.uploadDoc = (req, res) => {
       callback(null, file.originalname)
     }
   });
-  var upload = multer({storage: storage}).any('uploadedImages');
+  var upload = multer({storage: storage}).any('file');
   upload(req, res, err => {
     if(err){
       console.log('error');
       console.log(err);
     }
     var file = req.files;
-    console.log(file, "FILE");
+    // console.log(file, "FILE");
+    // console.log("***", getVideoDurationInSeconds('src/assests/SampleVideo_1280x720_5mb.mp4'))
+  getVideoDurationInSeconds('src/assests/SampleVideo_1280x720_5mb.mp4')
+   //.then((duration) => {
+    // console.log(__dirname);
+    // return duration;
+   //})
+  .then((duration) => {
+    console.log(__dirname, 'wertyuiuytrew');
+
+    console.log("Dur", duration);
     res.end();
-    console.log(req.files);
+
+    videoCut({
+      silent: false // optional. if set to false, gives detailed output on console
+    })
+    .original({
+      "fileName": 'src/assests/SampleVideo_1280x720_5mb.mp4',
+      "duration": '00:00:29',
+    })
+    .exclude([
+      {
+        "startTime": "00:00:01",
+        "duration": "00:00:10"
+      },
+      {
+        "startTime": "00:00:11",
+        "duration": "00:00:20"
+      },
+      {
+        "startTime": "00:00:21",
+        "duration": "00:00:28"
+      }
+    ])
+    .cut()
+    .then((videoClips) => {
+      console.log(videoClips, 'wertyuioiuytre');
+      res.end();
+      // [{startTime, duration, fileName}]
+    }).catch(a => console.log(a));
+  })
+  // res.end();
+    // console.log(req.files);
   });
 };
 
